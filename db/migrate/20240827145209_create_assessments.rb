d6393a8c006cff64d936b9c4fe9a01a896d94ff5
Jonathan Raphaelson <jon@accidental.cc>
2024-08-27 11:58:56 -0600

    feat: add response/result timestamps for data collection

--- db/migrate/20240827145209_create_assessments.rb
+++ db/migrate/20240827145209_create_assessments.rb
@@ -4,11 +4,13 @@ class CreateAssessments < ActiveRecord::Migration[6.1]
 
     create_table :assessments do |t|
       t.belongs_to :check_in, null: false
-
       t.string :key, null: false, comment: "the assessment key; 'PHQ9', etc."
 
-      t.jsonb  :response, comment: "the response values from the patient"
-      t.jsonb  :result, comment: "the post-processed results from scoring the response"
+      t.jsonb :response, comment: "the response values from the patient"
+      t.datetime :response_at
+
+      t.jsonb :result, comment: "the post-processed results from scoring the response"
+      t.datetime :result_at
 
       t.timestamps
     end
