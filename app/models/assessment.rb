a28df7181d323f271a89d04ac06c5bb7324b5b1a
Jonathan Raphaelson <jon@accidental.cc>
2024-08-27 11:18:00 -0600

    feat: basic assessment model
    the subclass will contain question and result schema validators

---
+++ app/models/assessment.rb
@@ -0,0 +1,13 @@
+class Assessment < ApplicationRecord
+  belongs_to :check_in
+
+  self.inheritance_column = :key
+  self.store_full_sti_class = false
+
+  scope :of_key, ->(key) { where(key:) }
+  scope :with_response, ->() { where.not(response: nil) }
+  scope :with_result,   ->() { where.not(result: nil) }
+
+  validates :check_in_id, presence: true
+  validates :key,         presence: true, inclusion: { in: %w[PHQ9] }
+end
---
+++ db/migrate/20240827145209_create_assessments.rb
@@ -0,0 +1,16 @@
+class CreateAssessments < ActiveRecord::Migration[6.1]
+  def change
+    # TODO: Rails 7 upgrade, switch key to an enumerated type
+
+    create_table :assessments do |t|
+      t.belongs_to :check_in, null: false
+
+      t.string :key, null: false, comment: "the assessment key; 'PHQ9', etc."
+
+      t.jsonb  :response, comment: "the response values from the patient"
+      t.jsonb  :result, comment: "the post-processed results from scoring the response"
+
+      t.timestamps
+    end
+  end
+end
