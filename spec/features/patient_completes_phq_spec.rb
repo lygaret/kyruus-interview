7b0e2fa6014864caf333c73b0f95e23dbf393cbc
Jonathan Raphaelson <jon@accidental.cc>
2024-08-27 20:00:25 -0600

    test: update specs to include answering the assessment

--- spec/features/patient_completes_phq_spec.rb
+++ spec/features/patient_completes_phq_spec.rb
@@ -11,5 +11,20 @@ RSpec.feature "A patient checks into the app" do
     click_on "Start PHQ screener"
 
     expect(page).to have_content("Over the past 2 weeks, how often have you been bothered by any of the following problems?")
+    expect(page).to have_content("Little interest or pleasure in doing things?")
+    expect(page).to have_content("Feeling down, depressed or hopeless?")
+
+    within :css, "select[data-qid='1']" do
+      select "Nearly every day"
+    end
+
+    within :css, "select[data-qid='2']" do
+      select "More than half the days"
+    end
+
+    click_on "Save"
+
+    expect(page).to have_content "According to the PHQ screener"
+    expect(page).to have_content "you should complete additional screening."
   end
 end
